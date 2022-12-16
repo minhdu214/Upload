73a3c7c3b5e6ee0b10ffaeed6d0cd4bd17a3b0b1
minhdu214 <minhdu214@gmail.com>
2022-12-16 15:06:18 +0700

    commit

---
+++ Test Suites/helo test suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>helo test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d01d81ce-e723-490c-99c5-92a3dedd7a17</testSuiteGuid>
+   <testCaseLink>
+      <guid>3fffc3be-3221-46d3-9046-1a42b5b7401c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test upload</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
