81e05ab54be6f4b65bb3043e6918c8cba35463b3
Chriskatalon <quan.trinh@katalon.com>
2023-01-03 10:39:08 +0700

    commit

--- Scripts/New Test Case/Script1671717974014.groovy
+++ Scripts/New Test Case/Script1671717974014.groovy
@@ -16,18 +16,12 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
-WebUI.openBrowser('')
+WebUI.openBrowser('google.com')
 
-WebUI.navigateToUrl('https://www.w3schools.com/howto/howto_html_file_upload_button.asp')
+WebUI.getUrl()
 
-//WebUI.uploadFile(findTestObject('Page_How To Create a File Upload Button/input_Next_myFile'), '10.png')
+WebUI.navigateToUrl('google.com')
 
+WebUI.click(findTestObject(null))
 
-def filePath = RunConfiguration.getProjectDir() + '/10.png'
-
-new File(filePath).getCanonicalPath()
-WebUI.uploadFile(findTestObject('Object Repository/Page_How To Create a File Upload Button/input_Next_myFile'), filePath)
