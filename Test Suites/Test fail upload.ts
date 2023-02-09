71d72a37fe5e8d2efd04d07b04c47a9650ed9cfe
Chriskatalon <quan.trinh@katalon.com>
2023-02-09 10:42:04 +0700

    commit

---
+++ Test Suites/Test fail upload.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test fail upload</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3b9e2ca7-5283-4c14-bf83-4a28cebd2091</testSuiteGuid>
+   <testCaseLink>
+      <guid>b56d9d01-e769-449c-b88c-35a2e9c5cdc1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
